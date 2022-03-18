c48d50e6cc8a7c19e2928233589731375cecfeb8
Widdy LOUIS <louisws@outlook.fr>
2022-03-18 08:53:08 +0100

    get all products

--- app.js
+++ app.js
@@ -17,9 +17,20 @@ let cart = [];
 class Products {
   async getProducts() {
     try {
+
       let result = await fetch('./products.json')
-      let data = result.json()
-      return data;
+      let data = await result.json()
+      //return data;
+      let products = data.items;
+      products = products.map( item => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return {title, price, id, image}
+      })
+
+      return products
+
     } catch (error) {
       console.log(error);
     }
@@ -28,7 +39,31 @@ class Products {
 
 // afficher les produits
 class UI {
+  displayProducts(products) {
+    console.log(products, ':)');
+    let result = '';
+    products.forEach( product => {
+      result += `
+        <!-- single products -->
+        <article class="product">
+          <div class="img-container">
+            <img src=${product.image}
+            alt="Product" 
+            class="product-img">
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>${product.price}€</h4>
+        </article>
+        <!-- single products -->
+      `
+    });
 
+    productsDOM.innerHTML = result;
+  }
 }
 
 // local storage
@@ -39,7 +74,7 @@ class Storage {
 document.addEventListener('DOMContentLoaded', ()=> {
   const ui = new UI();
   const products = new Products();
-
- products.getProducts().then( data => console.log(data))
+  // obtenir tous les produits
+ products.getProducts().then( products => ui.displayProducts( products))
   
 })
